905ae2b82c46b0b4418459860609c18f47066af4
DwayneL <dr2@ameritech.net>
2022-03-20 17:15:55 -0500

    added submit functionality to book form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -3,17 +3,28 @@ import React, { useState } from 'react'
 const BookForm = () => {
     const [book, setBook] = useState('')
 
+    const handleSubmit = (e) => {
+        // prevent default stops the page from refreshing
+        e.preventDefault()
+
+       console.log(book)
+    }
+
     return (
         <div>
             <h2>Add a Book</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Book Title</label>
                 <input
                 type='text'
                 required
                 value={book}
-                onChange = {(e) => setBook(e.target.ariaValueMin)}
+                onChange = {(e) => setBook(e.target.value)}
                 />
+
+                <button>
+                    Submit
+                </button>
             </form>
 
             <p>your book title is { book }</p>
